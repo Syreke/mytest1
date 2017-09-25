65e41709f4a71e63f997a23264407826352c1149
Syrym Seydazimov <syreken.ss@gmail.com>
2017-09-26 02:07:00 +0600

    The first commit

---
+++ src/kz/kaznitu/lessons/Hello.java
@@ -0,0 +1,10 @@
+package kz.kaznitu.lessons;
+
+/**
+ * Created by User on 26.09.2017.
+ */
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+}
