ab2ed2a14d1e386338208fb96a6f98eab9c5d644
Syrym Seydazimov <syreken.ss@gmail.com>
2017-09-26 02:12:24 +0600

    Озгерис жасалды

--- src/kz/kaznitu/lessons/Hello.java
+++ src/kz/kaznitu/lessons/Hello.java
@@ -1,10 +1,11 @@
 package kz.kaznitu.lessons;
-
 /**
  * Created by User on 26.09.2017.
  */
 public class Hello {
     public static void main(String[] args){
         System.out.println("Hello World!");
+        System.out.println("Бұл зертханалық жұмыстың мақсаты!");
+        System.out.println("Github - пен жумыс жасап уйрену");
     }
 }
