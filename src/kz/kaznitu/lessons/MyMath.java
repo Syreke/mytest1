8532a8003c425754206578bebc4e5a71502206b5
Syrym Seydazimov <syreken.ss@gmail.com>
2017-09-26 02:20:53 +0600

    The third commit

---
+++ src/kz/kaznitu/lessons/MyMath.java
@@ -0,0 +1,13 @@
+package kz.kaznitu.lessons;
+
+/**
+ * Created by User on 26.09.2017.
+ */
+public class MyMath {
+    public static  int kvadrat(int a) {
+        return a*a;
+    }
+    public static  int kub(int a) {
+        return a*a*a;
+    }
+}
