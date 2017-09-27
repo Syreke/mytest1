2ee8167616b17e11aa3c877a65a7ef297becaa35
Syrym Seydazimov <syreken.ss@gmail.com>
2017-09-27 13:45:27 +0600

    MyMath класымен байланыстырылды

--- src/kz/kaznitu/lessons/Hello.java
+++ src/kz/kaznitu/lessons/Hello.java
@@ -7,5 +7,11 @@ public class Hello {
         System.out.println("Hello World!");
         System.out.println("Бұл зертханалық жұмыстың мақсаты!");
         System.out.println("Github - пен жумыс жасап уйрену");
+
+        int a = MyMath.kvadrat(5);
+        int b = MyMath.kub(5);
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
     }
 }
