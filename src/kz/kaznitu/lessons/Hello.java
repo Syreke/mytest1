ebc9d2abba2fa82ffffd905f76ebf038c74b03e1
Syrym Seydazimov <syreken.ss@gmail.com>
2017-09-27 14:12:47 +0600

    experiment1 бұтағындағы Hello.java өзгеріс енгізілді

--- src/kz/kaznitu/lessons/Hello.java
+++ src/kz/kaznitu/lessons/Hello.java
@@ -8,11 +8,7 @@ public class Hello {
         System.out.println("Бұл зертханалық жұмыстың мақсаты!");
         System.out.println("Github - пен жумыс жасап уйрену");
 
-        int a = MyMath.kvadrat(5);
-        int b = MyMath.kub(5);
-
-        System.out.println("a = " + a);
-        System.out.println("b = " + b);
-        System.out.println(MyMath.max3(8,9,12));
+        System.out.println("max = " + MyMath.max3(8,9,12));
+        System.out.println("min = " + MyMath.min3(12,8,15));
     }
 }
